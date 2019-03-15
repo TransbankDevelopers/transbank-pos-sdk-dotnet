bc6e6d9d461f6664c7c4c2fac2a7f8ffad5b9001
afiebig <alfredo.fiebig@continuum.cl>
2019-03-15 12:39:17 -0300

    C Library now handles the configuration, and selection of the port

--- TransbankPosSDK/POS.cs
+++ TransbankPosSDK/POS.cs
@@ -9,39 +9,11 @@ namespace Transbank.POS
         public string Port { get; set; }
         private bool _configured = false;
 
-        private void SelectPort(string portName)
+        public void OpenPort( string portName, TbkBaudrate baudrate)
         {
             try
             {
-                if (TransbankWrap.select_port(portName) == tbk_return.TBK_NOK)
-                    throw new TransbankException("Unable to select specified port: " + portName);
-            }
-            catch (Exception e)
-            {
-                throw new TransbankException("Unable to select specified port: " + portName, e);
-            }
-        }
-
-        private void ConfigurePortBaudrate(tbk_baudrate baudrate)
-        {
-            try
-            {
-                if (TransbankWrap.configure_port((int)baudrate) == tbk_return.TBK_NOK)
-                    throw new TransbankException("Unable to set the specified baudrate: " + baudrate);
-            }
-            catch (Exception e)
-            {
-                throw new TransbankException("Unable to set the specified baudrate: " + baudrate, e);
-            }
-        }
-
-        public void OpenPort( string portName, tbk_baudrate baudrate)
-        {
-            try
-            {
-                SelectPort(portName);
-                ConfigurePortBaudrate(baudrate);
-                if (TransbankWrap.open_configured_port() == tbk_return.TBK_OK)
+                if (TransbankWrap.open_port(portName, (int)baudrate) == TbkReturn.TBK_OK)
                     _configured = true;
                 else
                     throw new TransbankException("Unable to Open selected port: " + portName);
