5e602d4847340eb32fae547a3e37ff4750cdeb0c
afiebig <alfredo.fiebig@continuum.cl>
2019-04-10 11:38:48 -0400

    Remove duplicated code from RegisterCloseResponse
    instead inherits from LoadKeysResponse

--- TransbankPosSDK/Responses/RegisterCloseResponse.cs
+++ TransbankPosSDK/Responses/RegisterCloseResponse.cs
@@ -2,19 +2,8 @@
 
 namespace Transbank.POS.Responses
 {
-    public class RegisterCloseResponse
+    public class RegisterCloseResponse : LoadKeysResponse
     {
-        public LoadKeyCloseResponse Response {get;}
-
-        public int FunctionCode => Response.function;
-        public string Result => ResponseCodes.Map[Response.responseCode];
-        public bool Sucess => ResponseCodes.Map[0].Equals(Result);
-        public long CommerceCode => Response.commerceCode;
-        public int TerminalId => Response.terminalId;
-
-        public RegisterCloseResponse(LoadKeyCloseResponse cresponse)
-        {
-            Response = cresponse;
-        }
+        public RegisterCloseResponse(LoadKeyCloseResponse cresponse) : base(cresponse) {}
     }
 }
