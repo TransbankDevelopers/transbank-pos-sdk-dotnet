acd03a5ca9fefb800ef4cb2b4abf35d8227ed899
afiebig <alfredo.fiebig@continuum.cl>
2019-10-22 17:38:37 -0300

    Add OnepayPayment Class implementi IOnepayPayment in TransbankSDK

---
+++ TransbankPosSDK/OnepayPayment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Transbank.Onepay;
+using Transbank.Onepay.Enums;
+using Transbank.Onepay.Model;
+using Transbank.Onepay.Exceptions;
+using Transbank.POS.Model;
+
+namespace Transbank.POS
+{
+    public class OnepayPayment : IOnepayPayment
+    {
+        public void Connected()
+        {
+        }
+
+        public void Disconnected()
+        {
+        }
+
+        public void NewMessage(string payload)
+        {
+        }
+
+    }
+}
