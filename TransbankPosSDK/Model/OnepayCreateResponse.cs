c6e67b2155c35b5dc50f5da6b78b10e09fd7a5a1
afiebig <alfredo.fiebig@continuum.cl>
2019-10-22 17:32:22 -0300

    Add class to enxapsulate OnepayCreate Responses
    this way it is not necesary to include TransbankSDK in the final proyect

---
+++ TransbankPosSDK/Model/OnepayCreateResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Transbank.Onepay.Model;
+
+namespace Transbank.POS.Model
+{
+    public class OnepayCreateResponse
+    {
+        private readonly TransactionCreateResponse response;
+        public string Ott { get => response.Ott.ToString(); }
+        public string Occ { get => response.Occ; }
+        public string ExternalUniqueNumber { get => response.ExternalUniqueNumber; }
+        public string QrCodeAsBase64 { get => response.QrCodeAsBase64; }
+
+        public OnepayCreateResponse(TransactionCreateResponse response)
+        {
+            this.response = response;
+        }
+    }
+}
