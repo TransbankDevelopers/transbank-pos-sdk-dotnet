bc9e18ac053c7e4f42395da11e2cae004bdbed76
afiebig <alfredo.fiebig@continuum.cl>
2019-10-22 17:41:15 -0300

    Add method to start a Onepay OnepayPayment
    This method create the Onepay cart with one item with the provided total
    Also, cretes the constructor for this class

--- TransbankPosSDK/OnepayPayment.cs
+++ TransbankPosSDK/OnepayPayment.cs
@@ -11,6 +11,27 @@ namespace Transbank.POS
 {
     public class OnepayPayment : IOnepayPayment
     {
+        private ShoppingCart cart;
+        private Websocket ws;
+
+        public int Ticket { get; }
+        public int Total { get; }
+        public string ExternalUniqueNumber { get; }
+
+        public string Occ { get; private set; }
+        public string Ott { get; private set; }
+
+        public OnepayPayment(int ticket, int total): 
+            this (ticket, total, Guid.NewGuid().ToString()){}
+
+        public OnepayPayment(int ticket, int total, string externalUniqueNumber)
+        {
+            Onepay.Onepay.CallbackUrl = "https://www.continuum.cl";
+            Ticket = ticket;
+            Total = total;
+            ExternalUniqueNumber = externalUniqueNumber;
+        }
+
         public void Connected()
         {
         }
@@ -21,6 +42,18 @@ namespace Transbank.POS
 
         public void NewMessage(string payload)
         {
+        }
+
+        public OnepayCreateResponse StarPayment()
+        {
+            cart = new ShoppingCart();
+            cart.Add(new Item(Ticket.ToString(), 1, Total, "", -1));
+
+            var response = Transaction.Create(cart, ChannelType.Mobile, ExternalUniqueNumber);
+            Occ = response.Occ;
+            Ott = response.Ott.ToString();
+
+            return new OnepayCreateResponse(response);
         }
 
     }
