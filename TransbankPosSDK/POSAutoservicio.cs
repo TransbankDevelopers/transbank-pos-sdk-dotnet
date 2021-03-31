4c14d80c9378872711a14ac11b919e21703bc15c
DarkFrost <m.astudillo.toledo@gmail.com>
2021-03-31 19:31:10 -0300

    add Poll method in POSAutoservicio class

--- TransbankPosSDK/POSAutoservicio.cs
+++ TransbankPosSDK/POSAutoservicio.cs
@@ -1,10 +1,49 @@
 ﻿using System;
-namespace Transbank.POS
+using System.Collections.Generic;
+using Transbank.POS.Utils;
+using Transbank.POS.IntegradoResponses;
+using Transbank.POS.IntegradoExceptions;
+using Transbank.POS.CommonResponses;
+using Transbank.POS.CommonExceptions;
+
+namespace Transbank.POSAutoservicio
 {
-    public class POSAutoservicio
+    public class POSAutoservicio : Serial
     {
         public POSAutoservicio()
         {
+
         }
+
+        public static POSAutoservicio Instance { get; } = new POSAutoservicio();
+
+        private void DiscardBuffer()
+        {
+            Port.DiscardInBuffer();
+            Port.DiscardOutBuffer();
+        }
+
+        public bool Poll()
+        {
+            DiscardBuffer();
+
+            if (CantWrite())
+            {
+                throw new TransbankException($"Unable to Poll port {Port.PortName} is closed");
+            }
+
+            try
+            {
+                byte[] buffer = new byte[1];
+                Port.Write("0100");
+                Port.BaseStream.ReadAsync(buffer, 0, 1).Wait();
+                return buffer[0] == ACK;
+            }
+            catch (Exception e)
+            {
+                throw new TransbankException($"Unable to send Poll command on port {Port.PortName}", e);
+            }
+        }
+
     }
 }
