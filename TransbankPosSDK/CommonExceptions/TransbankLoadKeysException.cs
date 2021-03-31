deb3c3f66657a1eea7313856b116f051b0dc6e72
DarkFrost <m.astudillo.toledo@gmail.com>
2021-03-31 17:24:56 -0300

    create folder for common response and exceptions and the files are moved. The file POS.cs is renamed to POSIntegrado.cs

---
+++ TransbankPosSDK/CommonExceptions/TransbankLoadKeysException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Transbank.POS.Responses;
+
+namespace Transbank.POS.Exceptions
+{
+    public class TransbankLoadKeysException : TransbankException
+    {
+        public LoadKeysResponse LoadKeyResponse;
+
+        public TransbankLoadKeysException(string message, LoadKeysResponse response) : base(message)
+        {
+            LoadKeyResponse = response;
+        }
+
+        public TransbankLoadKeysException(string message, Exception inner) : base(message, inner) { }
+    }
+}
