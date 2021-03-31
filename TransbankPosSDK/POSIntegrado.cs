4c6febb90edf34ce9be97582d623ea65d282b855
DarkFrost <m.astudillo.toledo@gmail.com>
2021-03-31 17:27:32 -0300

    remove POSIntegrado.cs methods corresponding to the handling of the serial port

--- TransbankPosSDK/POSIntegrado.cs
+++ TransbankPosSDK/POSIntegrado.cs
@@ -1,82 +1,22 @@
 ﻿using System;
-using System.Text;
-using System.IO.Ports;
-using System.Threading;
-using System.Threading.Tasks;
-using Transbank.POS.Responses;
-using Transbank.POS.Exceptions;
+using Transbank.POS.IntegradoResponses;
+using Transbank.POS.IntegradoExceptions;
+using Transbank.POS.Utils;
 using System.Collections.Generic;
+using Transbank.POS.CommonResponses;
+using Transbank.POS.CommonExceptions;
 
-namespace Transbank.POS
+namespace Transbank.POSIntegrado
 {
-    public class POS
+    public class POSIntegrado : Serial
     {
-        private static readonly byte ACK = 0x06;
-        private static readonly int _defaultTimeout = 150000;
-        private string _currentResponse;
-        public event EventHandler<IntermediateResponse> IntermediateResponseChange;
-        private string CurrentResponse
+        private POSIntegrado()
         {
-            get { return _currentResponse; }
-            set
-            {
-                _currentResponse = value;
-                if (_currentResponse.Length >= 1 && _currentResponse.Substring(1).Split('|')[0] == "0900")
-                {
-                    OnIntermediateMessageReceived(CurrentResponse);
-                }
-            }
-        }
-        private List<string> SaleDetail;
-
-        protected virtual void OnIntermediateMessageReceived(string message)
-        {
-            IntermediateResponseChange?.Invoke(this, new IntermediateResponse(message));
+            
         }
 
-        public bool IsPortOpen => Port.IsOpen;
-
-        public SerialPort Port { get; private set; }
+        public static POSIntegrado Instance { get; } = new POSIntegrado();
         
-
-        private POS()
-        {
-        }
-
-        public static POS Instance { get; } = new POS();
-
-        public void OpenPort(string portName, int baudrate = 115200)
-        {
-            if (Port != null && Port.IsOpen) ClosePort();
-            try
-            {
-                Port = new SerialPort(portName, baudrate, Parity.None, 8, StopBits.One);
-                Port.Open();
-                Port.ReadTimeout = _defaultTimeout;
-                Port.DiscardInBuffer();
-                Port.DiscardOutBuffer();
-            }
-            catch (Exception e)
-            {
-                throw new TransbankException("Could not Open Serial Port: " + portName, e);
-            }
-        }
-
-        public void ClosePort()
-        {
-            if (!Port.IsOpen) return;
-            try
-            {
-                Port.DiscardInBuffer();
-                Port.DiscardOutBuffer();
-                Port.Close();
-            }
-            catch (Exception e)
-            {
-                throw new TransbankException("Could not Close Serial Port: " + Port.PortName, e);
-            }
-        }
-
         [ObsoleteAttribute("This method is obsolete. Call SaleResponse(int, string) instead.", false)]
         public SaleResponse Sale(int amount, int ticket)
         {
@@ -255,7 +195,7 @@ namespace Transbank.POS
             Port.DiscardInBuffer();
             Port.DiscardOutBuffer();
 
-            if (Instance.CantWrite())
+            if (CantWrite())
             {
                 throw new TransbankException($"Unable to Poll port {Port.PortName} is closed");
             }
@@ -263,7 +203,7 @@ namespace Transbank.POS
             {              
                 byte[] buffer = new byte[1];
                 Port.Write("0100");
-                Instance.Port.BaseStream.ReadAsync(buffer, 0, 1).Wait();
+                Port.BaseStream.ReadAsync(buffer, 0, 1).Wait();
                 return buffer[0] == ACK;
             }
             catch (Exception e)
@@ -277,7 +217,7 @@ namespace Transbank.POS
             Port.DiscardInBuffer();
             Port.DiscardOutBuffer();
 
-            if (Instance.CantWrite())
+            if (CantWrite())
             {
                 throw new TransbankException($"Unable to Set Normal Mode port {Port.PortName} is closed");
             }
@@ -285,141 +225,13 @@ namespace Transbank.POS
             {
                 byte[] buffer = new byte[1];
                 Port.Write("0300\0");
-                Instance.Port.BaseStream.ReadAsync(buffer, 0, 1).Wait();
+                Port.BaseStream.ReadAsync(buffer, 0, 1).Wait();
                 return buffer[0] == ACK;
             }
             catch (Exception e)
             {
                 throw new TransbankException($"Unable to send Normal Mode command on port {Port.PortName}", e);
             }
-        }
-
-        private async Task WriteData(string payload, bool intermediateMessages = false, bool saleDetail = false, bool printOnPOS = true)
-        {
-            Port.DiscardInBuffer();
-            Port.DiscardOutBuffer();
-
-            CurrentResponse = "";
-            if (Instance.CantWrite())
-            {
-                throw new TransbankException($"Unable to send message to {Port.PortName}");
-            }
-            Port.Write(payload);
-            bool ack = ReadAck(new CancellationTokenSource(_defaultTimeout).Token);
-            
-            if (ack)
-            {
-                Console.WriteLine("Read ACK OK");
-                if (intermediateMessages)
-                {
-                    await ReadMessage(new CancellationTokenSource(_defaultTimeout).Token);
-                    string responseCode = CurrentResponse.Substring(1).Split('|')[1];
-                    while (responseCode == "78" || responseCode == "79" ||
-                        responseCode == "80" || responseCode == "81" || responseCode == "82")
-                    {
-                        await ReadMessage(new CancellationTokenSource(_defaultTimeout).Token);
-                        responseCode = CurrentResponse.Substring(1).Split('|')[1];
-                    }
-                }
-                else
-                {
-                    if (saleDetail)
-                    {
-                        SaleDetail = new List<string>();
-                        if (!printOnPOS)
-                        {
-                            await ReadMessage(new CancellationTokenSource(_defaultTimeout).Token);
-                            string authorizationCode;
-                            try
-                            {
-                                authorizationCode = CurrentResponse.Substring(1).Split('|')[5];
-                                if (authorizationCode != "" && authorizationCode != " ")
-                                {
-                                    SaleDetail.Add(string.Copy(CurrentResponse));
-                                }
-                            }
-                            catch (IndexOutOfRangeException)
-                            {
-                                authorizationCode = null;
-                            }
-
-                            while (authorizationCode != null && authorizationCode != "" && authorizationCode != " ")
-                            {
-                                await ReadMessage(new CancellationTokenSource(_defaultTimeout).Token);
-                                try
-                                {
-                                    authorizationCode = CurrentResponse.Substring(1).Split('|')[5];
-                                    if (authorizationCode != "" && authorizationCode != " ")
-                                    {
-                                        SaleDetail.Add(string.Copy(CurrentResponse));
-                                    }
-                                }
-                                catch (IndexOutOfRangeException)
-                                {
-                                    authorizationCode = null;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        await ReadMessage(new CancellationTokenSource(_defaultTimeout).Token);
-                    }
-                }
-            }
-            else
-            {
-                throw new TransbankException($"Unable to send message to {Port.PortName}");
-            }
-        }
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        private async Task ReadMessage(CancellationToken token)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-        {
-            while (!token.IsCancellationRequested && Port.BytesToRead <= 0)
-            {
-            }
-            if (token.IsCancellationRequested)
-            {
-                throw new TransbankException($"Read operation Timeout");
-            }
-
-            Instance.Port.ReadTo("");
-            CurrentResponse = "" + Instance.Port.ReadTo("");
-            Instance.Port.Write("");
-            Console.WriteLine(CurrentResponse);
-        }
-
-        private bool ReadAck(CancellationToken token)
-        {
-            while (!token.IsCancellationRequested && Port.BytesToRead <= 0)
-            {
-            }
-            if (token.IsCancellationRequested)
-            {
-                throw new TransbankException($"Read operation Timeout");
-            }
-            byte[] result = new byte[1];
-            Instance.Port.BaseStream.ReadAsync(result, 0, 1, token).Wait();
-            return result[0] == ACK;
-        }
-
-        private bool CantWrite() => Port == null || !Port.IsOpen;
-
-        private string MessageWithLRC(string message)
-        {
-            return message + Lrc(message);
-        }
-
-        private char Lrc(string message)
-        {
-            int lrc = 0;
-            for (int i = 1; i < message.Length; i++)
-            {
-                lrc ^= Encoding.ASCII.GetBytes(message.Substring(i, 1))[0];
-            }
-            return (char)lrc;
         }
     }
 }
