f39a9d3a61f7ac093a81827449d6437e9c874b91
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-06 17:50:48 -0400

    create autoservicio multicodesale response

---
+++ TransbankPosSDK/Responses/AutoservicioResponse/MultiCodeSaleResponse.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Transbank.POS.Responses.AutoservicioResponse
+{
+    public class MultiCodeSaleResponse : SaleResponse
+    {
+        protected new readonly Dictionary<string, int> ParameterMap = new Dictionary<string, int>
+        {
+            { "CommerceProviderCode", 14},
+            { "SharesType", 15},
+            { "SharesNumber", 17},
+            { "SharesAmount", 18},
+            { "SharesTypeGloss", 19}
+        };
+
+        public int CommerceProviderCode
+        {
+            get
+            {
+                try
+                {
+                    int.TryParse(Response.Split('|')[ParameterMap["CommerceProviderCode"]].Trim(), out int commerceProviderCode);
+                    return commerceProviderCode;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return -1;
+                }
+            }
+        }
+        public new int SharesType
+        {
+            get
+            {
+                try
+                {
+                    int.TryParse(Response.Split('|')[ParameterMap["SharesType"]].Trim(), out int sharesType);
+                    return sharesType;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return -1;
+                }
+            }
+        }
+        public new int SharesNumber
+        {
+            get
+            {
+                try
+                {
+                    int.TryParse(Response.Split('|')[ParameterMap["SharesNumber"]].Trim(), out int SharesNumber);
+                    return SharesNumber;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return -1;
+                }
+            }
+        }
+        public new int SharesAmount
+        {
+            get
+            {
+                try
+                {
+                    int.TryParse(Response.Split('|')[ParameterMap["SharesAmount"]].Trim(), out int sharesAmount);
+                    return sharesAmount;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return -1;
+                }
+            }
+        }
+        public new string SharesTypeGloss
+        {
+            get
+            {
+                try
+                {
+                    return Response.Split('|')[ParameterMap["SharesTypeGloss"]].Trim();
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return "";
+                }
+            }
+        }
+
+        public MultiCodeSaleResponse(string response) : base(response) { }
+
+        public override string ToString()
+        {
+            string formatedAccountingDate = AccountingDate.HasValue ? AccountingDate.Value.ToString("dd/MM/yyyy hh:mm:ss") : "";
+            string formatedRealDate = RealDate.HasValue ? RealDate.Value.ToString("dd/MM/yyyy hh:mm:ss") : "";
+            return "Function: " + FunctionCode + "\n" +
+                   "Response: " + ResponseMessage + "\n" +
+                   "Commerce Code: " + CommerceCode + "\n" +
+                   "Terminal Id: " + TerminalId + "\n" +
+                   "Ticket: " + Ticket + "\n" +
+                   "AuthorizationCode Code: " + AuthorizationCode + "\n" +
+                   "Amount: " + Amount + "\n" +
+                   "Last 4 Digits: " + Last4Digits + "\n" +
+                   "Operation Number: " + OperationNumber + "\n" +
+                   "Card Type: " + CardType + "\n" +
+                   "Accounting Date: " + formatedAccountingDate + "\n" +
+                   "Account Number: " + AccountNumber + "\n" +
+                   "Card Brand: " + CardBrand + "\n" +
+                   "Real Date: " + formatedRealDate + "\n" +
+                   "CommerceProviderCode: " + CommerceProviderCode + "\n" +
+                   "Shares Type: " + SharesType + "\n" +
+                   "Shares Number: " + SharesNumber + "\n" +
+                   "Shares Amount: " + SharesAmount + "\n" +
+                   "Shares Type Gloss: " + SharesTypeGloss;
+        }
+    }
+}
