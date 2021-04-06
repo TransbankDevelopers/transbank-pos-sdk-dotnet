ef6b20babc428babee6f1fffecadf86a123a8de3
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-06 17:51:27 -0400

    create autoservicio multicodesale exception

---
+++ TransbankPosSDK/Exceptions/AutoservicioExceptions/TransbankMultiCodeSaleException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Transbank.POS.Responses.AutoservicioResponse;
+
+namespace Transbank.POS.Exceptions.AutoservicioExceptions
+{
+    public class TransbankMultiCodeSaleException : TransbankSaleException
+    {
+        public TransbankMultiCodeSaleException(string message, MultiCodeSaleResponse response) : base(message)
+        {
+            SaleResponse = response;
+        }
+
+        public TransbankMultiCodeSaleException(string message, Exception inner) : base(message, inner) { }
+
+        public TransbankMultiCodeSaleException(string message) : base(message) { }
+    }
+}
