be1ea433ea384fe879371a282bc6e1434f1e0f87
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-06 11:07:52 -0400

    create sale exception for autoservicio

---
+++ TransbankPosSDK/Exceptions/AutoservicioExceptions/TransbankSaleException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Transbank.POS.Responses.AutoservicioResponse;
+
+namespace Transbank.POS.Exceptions.AutoservicioExceptions
+{
+    public class TransbankSaleException : CommonExceptions.TransbankException
+    {
+        public SaleResponse SaleResponse;
+
+        public TransbankSaleException(string message, SaleResponse response) : base(message)
+        {
+            SaleResponse = response;
+        }
+
+        public TransbankSaleException(string message, Exception inner) : base(message, inner) { }
+
+        public TransbankSaleException(string message) : base(message) { }
+    }
+}
