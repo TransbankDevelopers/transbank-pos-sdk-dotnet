852ddcc8049abe5708d3dff718dc0cffd3f3f024
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-07 13:23:54 -0400

    create autoservicio last sale exception

---
+++ TransbankPosSDK/Exceptions/AutoservicioExceptions/TransbankLastSaleException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Transbank.POS.Responses.AutoservicioResponse;
+
+namespace Transbank.POS.Exceptions.AutoservicioExceptions
+{
+    public class TransbankLastSaleException : CommonExceptions.TransbankException
+    {
+        public LastSaleResponse LastSaleResponse;
+
+        public TransbankLastSaleException(string message, LastSaleResponse response) : base(message)
+        {
+            LastSaleResponse = response;
+        }
+
+        public TransbankLastSaleException(string message, Exception inner) : base(message, inner) { }
+    }
+}
