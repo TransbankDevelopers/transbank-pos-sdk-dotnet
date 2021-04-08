be25bc22eda1ca4ba7442caa20911079a222cc9d
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-08 19:45:13 -0400

    create autoservicio close response

---
+++ TransbankPosSDK/Responses/AutoservicioResponse/CloseResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transbank.POS.Responses.AutoservicioResponse
+{
+    public class CloseResponse : CommonResponses.LoadKeysResponse
+    {
+        protected Dictionary<string, int> ParameterMap = new Dictionary<string, int>
+        {
+            { "PrintingField", 4},
+        };
+
+        public string PrintingField
+        {
+            get
+            {
+                try
+                {
+                    return Response.Split('|')[ParameterMap["PrintingField"]].Trim();
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    return "";
+                }
+            }
+        }
+
+        public CloseResponse(string response) : base(response) { }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +
+                   "Printing Field: " + PrintingField;
+        }
+    }
+}
