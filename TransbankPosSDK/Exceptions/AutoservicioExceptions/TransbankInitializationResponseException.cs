5b2d5f29be6d223c7855451a2c220bf9bdb42917
DarkFrost <m.astudillo.toledo@gmail.com>
2021-04-20 13:42:28 -0400

    add initialization response exception

---
+++ TransbankPosSDK/Exceptions/AutoservicioExceptions/TransbankInitializationResponseException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Transbank.Responses.AutoservicioResponse;
+
+namespace Transbank.Exceptions.AutoservicioExceptions
+{
+    public class TransbankInitializationResponseException : CommonExceptions.TransbankException
+    {
+        public InitializationResponse RegisterInitializationResponse;
+
+        public TransbankInitializationResponseException(string message, InitializationResponse response) : base(message)
+        {
+            RegisterInitializationResponse = response;
+        }
+
+        public TransbankInitializationResponseException(string message, Exception inner) : base(message, inner) { }
+
+        public TransbankInitializationResponseException(string message) : base(message) { }
+    }
+}
