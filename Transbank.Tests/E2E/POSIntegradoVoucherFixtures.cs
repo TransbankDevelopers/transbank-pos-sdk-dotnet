855b065affb612dd61ac4b8945fd03af3f57896a
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2026-04-15 19:29:13 +0000

    test: add pos integrado multicode debit e2e with voucher

--- Transbank.Tests/E2E/POSIntegradoVoucherFixtures.cs
+++ Transbank.Tests/E2E/POSIntegradoVoucherFixtures.cs
@@ -2,6 +2,50 @@ namespace Transbank.Tests.E2E
 {
     internal static class POSIntegradoVoucherFixtures
     {
+        internal static readonly string MultiCodeSaleDebitVoucher =
+            "               TRANSBANK                " +
+            "         VENTA - COPIA COMERCIO         " +
+            "           TARJETA DE DEBITO            " +
+            "              PRUEBA DESA               " +
+            "             Cerro El Plomo             " +
+            "           CIUDAD DESARROLLO            " +
+            "     597029414300-IT750050-I25.0A0      " +
+            "              597029414303              " +
+            "07/04/2026      08:50:34                " +
+            "                           ****3331 B-DB" +
+            "MONTO VENTA:                      $6.723" +
+            "IVA:                              $1.277" +
+            "TOTAL:                            $8.000" +
+            "MONEDA:                             PESO" +
+            "OPERACION: 000143   AUTORIZACION: 708410" +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "               TRANSBANK                " +
+            "         VENTA - COPIA CLIENTE          " +
+            "           TARJETA DE DEBITO            " +
+            "              PRUEBA DESA               " +
+            "             Cerro El Plomo             " +
+            "           CIUDAD DESARROLLO            " +
+            "     597029414300-IT750050-I25.0A0      " +
+            "              597029414303              " +
+            "07/04/2026      08:50:34                " +
+            "                           ****3331 B-DB" +
+            "MONTO VENTA:                      $6.723" +
+            "IVA:                              $1.277" +
+            "TOTAL:                            $8.000" +
+            "MONEDA:                             PESO" +
+            "OPERACION: 000143   AUTORIZACION: 708410" +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        " +
+            "                                        ";
+
         internal static readonly string MultiCodeSaleCreditVoucher =
             "               TRANSBANK                " +
             "     VENTA CON PIN - COPIA COMERCIO     " +
